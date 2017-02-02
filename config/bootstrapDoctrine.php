12b9205a0a28e3a272fc2d2ff95c393c8330fe65
B D <bdeglane@users.noreply.github.com>
2017-02-02 18:01:23 +0100

    add port

--- config/bootstrapDoctrine.php
+++ config/bootstrapDoctrine.php
@@ -7,21 +7,25 @@ use Doctrine\ORM\Tools\Setup;
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/settings.php";
 
+
+
 $isDevMode = true;
 
 // path to entities
 $entities = array(
-    __DIR__ . '\\..\\app\\Bundle\\UserBundle\\Model\\'
+    __DIR__ . '/../app/Bundle/UserBundle/Model'
 );
 $config = Setup::createAnnotationMetadataConfiguration($entities, $isDevMode);
 
 $conn = array(
     'driver' => _DBDRIVER,
-    'host' => _DBHOST . ':' . _DBPORT,
+    'host' => _DBHOST,
+    'port' => _DBPORT,
     'user' => _DBUSER,
     'password' => _DBPWD,
     'dbname' => _DBNAME
 );
 
 // save the entityManager in the database class
-Database::setEntityManager(EntityManager::create($conn, $config));
+$em = EntityManager::create($conn, $config);
+Database::setEntityManager($em);

80f6c75fcc556d525ca149a9fc537e49e1b48337
B D <bdeglane@users.noreply.github.com>
2017-02-02 11:43:26 +0100

    register User Bundle

--- app/Bundle/moduleIndex.php
+++ app/Bundle/moduleIndex.php
@@ -3,6 +3,9 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+
+require_once 'User/routing.php';
+
 $app->get('/hello/{name}', function (Request $req, Response $res, $args = []) {
     $simple = $res->withStatus(200);
     $json = $simple->withJson(['hello ' . $args['name']]);
